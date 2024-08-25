f97f9cefbb12b00fa3c36f733f0757668213c7d6
yonasghiwot <yonihabeshaw@gmail.com>
2024-08-25 07:37:32 -0700

    puppt indentation fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #manifest to fix Nginx open file limit configuration
 exec { 'nginx fix':
-  command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
-  path     => '/usr/local/bin/:usr/local/sbin/:/user/sbin/:/user/bin/:/sbin/:/bin/'
+  command => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
+  path    => '/usr/local/bin/:usr/local/sbin/:/user/sbin/:/user/bin/:/sbin/:/bin/'
 }
